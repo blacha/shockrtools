c429d9445eabc7d864d5658aba412552c75c668a
DrunphO <DrunphO@users.noreply.github.com>
2015-01-14 12:27:19 +0000

    Update button.js

--- client/modules/button.js
+++ client/modules/button.js
@@ -2,12 +2,11 @@
 
 var Button = {
     name: 'Button',
-
+    
     startup: function() {
         registerButtons();
         // Init the button
         ST.qx.main.getInstance();
-
         ST.util.button.setLabel('Scan');
     }
 };
@@ -20,21 +19,30 @@ function registerButtons() {
             this.button = new qx.ui.form.Button('Scan');
             this.button.set({
                 width: 100,
-                appearance: 'button-bar-center',
-                toolTipText: 'Scan'
+                appearance: 'button-bar-right',
+                toolTipText: 'Scan Layouts'
             });
-
+            
             ST.util.button = this.button;
             this.button.addListener('click', this.click, this);
             var mainBar = qx.core.Init.getApplication().getUIItem(ClientLib.Data.Missions.PATH.BAR_MENU);
-            mainBar.getChildren()[1].addAt(this.button, 8, {
-                top: 0,
-                right: 0
-            });
+            var childs = mainBar.getChildren()[1].getChildren();
+            
+            for( var z = childs.length - 1; z>=0;z--){
+                if( typeof childs[z].setAppearance === "function"){
+                    if( childs[z].getAppearance() == "button-bar-right"){
+                        childs[z].setAppearance("button-bar-center");
+                    }
+                }
+            }
+            
+            mainBar.getChildren()[1].add(this.button);
+            mainBar.getChildren()[0].setScale(true);
+            mainBar.getChildren()[0].setWidth(860);
+            
             console.log('ST:Util - Scan Button added');
         },
         members: {
-
             click: function() {
                 if (ST.BaseScanner.isScanning()) {
                     return ST.BaseScanner.abort();
@@ -42,7 +50,6 @@ function registerButtons() {
                 ST.BaseScanner.scan();
             }
         }
-
     });
 }
 
